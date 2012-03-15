ef083c45228ae76ee67f1bebb0a62c8ae875984a
Jyri Tuulos <jyri.tuulos@gmail.com>
2012-03-14 22:59:13 -0700

    add group changer and Bitdeli loader to food group visualization

--- web/js/groups.js
+++ web/js/groups.js
@@ -1,54 +1,93 @@
 $(function() {
 
-var w = 960, 
+var w = 960,
     h = 960,
     format = d3.format(",d"),
     fill = d3.scale.category20b();
 
-var data = [{
-  "name": "vegetables",
-  "nutrients": foodgroups[1100].foods
-}]
+// Load data from Bitdeli
+var sourceId = "i-04bb3fe3b92b1b-e31198a8",
+    groupsUrl = "https://out.bitdeli.com/v1/data/"+sourceId+"/groups/";
 
-$('h1').text(data[0].name);
+// Get group from Bitdeli
+function loadGroup(groupId) {
+  var items = [],
+      initialUrl = groupsUrl+groupId+"?max=100";
 
-var pack = d3.layout.pack()
-    .size([w - 4, h - 4])
-    .sort(null)
-    .value(function(d) { return d.amount; })
-    .children(function(d) {return d.nutrients;});
+  getWholeGroup(initialUrl, function(items) {
+    redraw([{
+      name: foodgroups[groupId],
+      nutrients: _(items).pluck("object")
+    }]);
+  });
 
-var vis = d3.select("#main").append("svg:svg")
-    .attr("width", w)
-    .attr("height", h)
-    .attr("class", "pack")
-    .append("svg:g")
-    .attr("transform", "translate(2, 2)");
+  function getWholeGroup(url, complete) {
+    $.getJSON(url, function(json) {
+      items.push.apply(items, json.items);
+      if (json.next) {
+        getWholeGroup(json.next, complete);
+      } else {
+        complete.call(this, items);
+      }
+    });
+  }
+}
 
-var node = vis.data(data).selectAll('g.node')
-    .data(pack.nodes)
-    .enter().append('svg:g')
-    .attr('transform', function(d) { return "translate(" + d.x + "," + d.y + ")";});
+// Redraw visualization
+function redraw(data) {
+  $("#main").empty();
+  $('h1').text(data[0].name);
 
-node.append("svg:title")
-    .text(function(d) {return d.name || nutrients[d.id].name});
+  var pack = d3.layout.pack()
+      .size([w - 4, h - 4])
+      .sort(null)
+      .value(function(d) { return d.amount; })
+      .children(function(d) {return d.nutrients;});
 
-node.append("svg:circle")
-    .attr("r", function(d) {return d.r})
-    .style("fill", function(d) {return _.isUndefined(nutrients[d.id]) ? "#aaa" : fill(nutrients[d.id].name)})
-    .attr("class", function(d) {
-     return _.isUndefined(nutrients[d.id]) ? "" : "nut c" + d.id;
-    })
-    .attr("fill-opacity", function(d) {return _.isUndefined(nutrients[d.id]) ? 0.5 : 0.7});
+  var vis = d3.select("#main").append("svg:svg")
+      .attr("width", w)
+      .attr("height", h)
+      .attr("class", "pack")
+      .append("svg:g")
+      .attr("transform", "translate(2, 2)");
 
-$('.nut').hover(function() {
-  var curC = $(this).attr("class").substr(4);
-  $('.'+curC).css({"fill-opacity":"1"});
-  $('h2').text(nutrients[parseFloat(curC.substr(1))].name);
-}, function() {
-  var curC = $(this).attr("class").substr(4);
-  $('.'+curC).css({"fill-opacity":".7"});
-  $('h2').text("");
+  var node = vis.data(data).selectAll('g.node')
+      .data(pack.nodes)
+      .enter().append('svg:g')
+      .attr('transform', function(d) { return "translate(" + d.x + "," + d.y + ")";});
+
+  node.append("svg:title")
+      .text(function(d) {return d.name || nutrients[d.id].name});
+
+  node.append("svg:circle")
+      .attr("r", function(d) {return d.r})
+      .style("fill", function(d) {return _.isUndefined(nutrients[d.id]) ? "#aaa" : fill(nutrients[d.id].name)})
+      .attr("class", function(d) {
+       return _.isUndefined(nutrients[d.id]) ? "" : "nut c" + d.id;
+      })
+      .attr("fill-opacity", function(d) {return _.isUndefined(nutrients[d.id]) ? 0.5 : 0.7});
+
+  // Attach mouse handlers
+  $('.nut').hover(function() {
+    var curC = $(this).attr("class").substr(4);
+    $('.'+curC).css({"fill-opacity":"1"});
+    $('h2').text(nutrients[parseFloat(curC.substr(1))].name);
+  }, function() {
+    var curC = $(this).attr("class").substr(4);
+    $('.'+curC).css({"fill-opacity":".7"});
+    $('h2').text("");
+  });
+}
+
+// Initialize group changer
+var changer = $("#group-changer");
+changer.append(_(foodgroups).map(function(name, id) {
+  return $("<option/>").text(name).attr("value", id)[0];
+})).change(function() {
+  loadGroup(changer.val());
 });
+
+// Set initial group
+changer.val(2200).change();
 
 });
